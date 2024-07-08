11d64d022bac2c1068e581003a2395d19f65c548
JT Archie <jtarchie@gmail.com>
2024-07-07 19:39:13 -0600

    remove pragma

--- executers/schema.sql
+++ executers/schema.sql
@@ -1,15 +1,3 @@
-PRAGMA journal_mode = WAL;
-
-PRAGMA synchronous = NORMAL;
-
-PRAGMA journal_size_limit = 67108864;
-
-PRAGMA mmap_size = 268435456;
-
-PRAGMA cache_size = 2000;
-
-PRAGMA busy_timeout = 5000;
-
 CREATE TABLE IF NOT EXISTS keys (
   name TEXT NOT NULL PRIMARY KEY,
   value TEXT,
